d929cfc36f78d1e0600e5818aee845661a4ed005
D0NN4 <sgarbidonna@gmail.com>
2024-08-26 11:08:21 -0300

    package json build en webpack --mode production (antes estaba como serve)

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
-//const TerserPlugin = require("terser-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  //entry: '/public/3Dconfiguration.ts',
-  entry: './public/index.ts',
+  entry: '/public/3Dconfiguration.ts',
+  //entry: './public/index.ts',
   mode: 'production',
-  /*
+  
   performance: {
     hints: false
   },
@@ -17,7 +17,7 @@ module.exports = {
         safari10: true
       }
     })],
-  },*/
+  },
   module: {
     rules: [{
       test: /\.tsx?$/,
@@ -30,16 +30,16 @@ module.exports = {
   },
   
   output: {
-    filename: 'bundle.js',
+    filename: 'ybundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  /*
+  
   plugins: [
       new HtmlWebpackPlugin({
         template: './public/3Dconfiguration.html',
         filename: '3Dconfiguration.html',
       }),
-  ]*/
+  ]
 };
 
 /*
