aff80e601de8dce673f48acfebf8320588ae1864
D0NN4 <sgarbidonna@gmail.com>
2024-09-04 14:55:14 -0300

    -it y -es en tablet, phone number en form

---
+++ public/contenido/3Dconfiguration/preload.js
@@ -0,0 +1,14 @@
+          
+          document.addEventListener("DOMContentLoaded", function() {
+            setTimeout(function() {
+              document.getElementById("preload-screen").style.display = "none"; 
+              }, 3000); 
+        });
+
+        document.addEventListener("DOMContentLoaded", function() {
+          setInterval(function() {
+              document.body.style.overflow = "visible"; }, 3000); 
+        });
+
+        
+    
