1cb1342d773d014ad0dcd4c5c7988b8093b79a91
D0NN4 <sgarbidonna@gmail.com>
2024-09-17 01:51:23 -0300

    rotation ipad3

--- public/contenido/3Dconfiguration/ipad-vertical.js
+++ public/contenido/3Dconfiguration/ipad-vertical.js
@@ -6,57 +6,54 @@ function isTabletOrIpadInPortrait() {
     // opcion alternativa p ver si estoy o no con portrait const isPortrait = window.orientation === 0 || window.orientation === 180;
   
     if (isIpadOrTablet && isPortrait) {
-      console.log("Estás utilizando una tablet o iPad en modo vertical.");
-      alert('si');
-            const sectionIntro = document.querySelector('.section-intro');
-            const canvas = document.querySelector('#canvas');
-            const sdControl = document.querySelector('.sd-control');
-            const menu = document.querySelector('#menu');
-            const intro3D = document.querySelector('.intro-3d');
-            const infoMenu = document.querySelector('#info-menu');
-            const patternColors = document.querySelector('#pattern-colors');
+        console.log("Estás utilizando una tablet o iPad en modo vertical.");
+        alert('si');
+    
+        const sectionIntro = document.querySelector('.section-intro');
+        const canvas = document.querySelector('#canvas');
+        const sdControl = document.querySelector('.sd-control');
+        const menu = document.querySelector('#menu');
+        const intro3D = document.querySelector('.intro-3d');
+        const infoMenu = document.querySelector('#info-menu');
+        const patternColors = document.querySelector('#pattern-colors');
 
+        sectionIntro.style.position = 'relative';
+        sectionIntro.style.maxWidth = '100vw';
+        sectionIntro.style.height = 'auto';
+        sectionIntro.style.padding = '15vh 3vw 12vh 3vw';
+        sectionIntro.style.flexDirection = 'column !important';
 
+        canvas.style.maxWidth = '80vw';
+        canvas.style.maxHeight = '49vw';
+        canvas.style.mixBlendMode = 'multiply';
+        //canvas.style.marginLeft = '16vw';
 
-            patternColors.style.position = 'relative';
-            patternColors.style.top = '-43vh';
-            patternColors.style.right = '-80vw';
-            patternColors.style.height = '0';
+        sdControl.style.gridTemplateColumns = '17% 64% 25%';
+
+        menu.style.fontSize = 'x-small';
+        intro3D.style.marginTop = '2em';
+        intro3D.style.display = 'grid';
+        intro3D.style.gridTemplateRows = '55%';
+        intro3D.style.height = 'auto';
+        intro3D.style.maxHeight = '100vh';
+
+        infoMenu.style.maxWidth = '100vw';
+        infoMenu.style.gridTemplateColumns = '45% 50%';
+        infoMenu.style.marginTop = '3vh';
+        infoMenu.style.marginBottom = '1vh';
+        infoMenu.style.height = '5vh';
+        infoMenu.style.display = 'grid';
+        infoMenu.style.alignContent = 'space-between';
+        infoMenu.style.justifyContent = 'space-evenly';
+        infoMenu.style.alignItems = 'center';
+        infoMenu.style.justifyItems = 'stretch';
+        infoMenu.style.marginLeft = '50px';
+
+        patternColors.style.position = 'relative';
+        patternColors.style.top = '-43vh';
+        patternColors.style.right = '-80vw';
+        patternColors.style.height = '0';
             
-
-            sectionIntro.style.position = 'relative';
-            sectionIntro.style.maxWidth = '100vw';
-            sectionIntro.style.height = 'auto';
-            sectionIntro.style.padding = '15vh 3vw 12vh 3vw';
-            sectionIntro.style.flexDirection = 'column';
-
-            canvas.style.maxWidth = '80vw';
-            canvas.style.maxHeight = '49vw';
-            canvas.style.mixBlendMode = 'multiply';
-            canvas.style.marginLeft = '16vw';
-
-            sdControl.style.gridTemplateColumns = '17% 64% 25%';
-
-            menu.style.fontSize = 'x-small';
-
-            intro3D.style.marginTop = '2em';
-            intro3D.style.display = 'grid';
-            intro3D.style.gridTemplateRows = '55%';
-            intro3D.style.height = 'auto';
-            intro3D.style.maxHeight = '100vh';
-
-            infoMenu.style.maxWidth = '100vw';
-            infoMenu.style.gridTemplateColumns = '45% 50%';
-            infoMenu.style.marginTop = '3vh';
-            infoMenu.style.marginBottom = '1vh';
-            infoMenu.style.height = '5vh';
-            infoMenu.style.display = 'grid';
-            infoMenu.style.alignContent = 'space-between';
-            infoMenu.style.justifyContent = 'space-evenly';
-            infoMenu.style.alignItems = 'center';
-            infoMenu.style.justifyItems = 'stretch';
-            infoMenu.style.marginLeft = '50px';
-
       return true;
     } else {
     }
