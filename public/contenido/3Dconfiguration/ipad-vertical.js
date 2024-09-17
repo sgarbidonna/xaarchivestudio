1accc52f8da170366ca8f7a19a80ca07ebe639c8
D0NN4 <sgarbidonna@gmail.com>
2024-09-17 00:23:07 -0300

    rotation ipad ver

--- public/contenido/3Dconfiguration/ipad-vertical.js
+++ public/contenido/3Dconfiguration/ipad-vertical.js
@@ -8,16 +8,53 @@ function isTabletOrIpadInPortrait() {
     if (isIpadOrTablet && isPortrait) {
       console.log("Estás utilizando una tablet o iPad en modo vertical.");
       //alert('si');
+      // Seleccionar los elementos
+            const sectionIntro = document.querySelector('.section-intro');
+            const canvas = document.querySelector('#canvas');
+            const sdControl = document.querySelector('.sd-control');
+            const menu = document.querySelector('#menu');
+            const intro3D = document.querySelector('.intro-3d');
+            const infoMenu = document.querySelector('#info-menu');
+
+            // Aplicar estilos con JavaScript
+            sectionIntro.style.position = 'relative';
+            sectionIntro.style.maxWidth = '100vw';
+            sectionIntro.style.height = 'auto';
+            sectionIntro.style.padding = '15vh 3vw 12vh 3vw';
+            sectionIntro.style.flexDirection = 'column';
+
+            canvas.style.maxWidth = '63vw';
+            canvas.style.maxHeight = '49vw';
+            canvas.style.mixBlendMode = 'multiply';
+            canvas.style.marginLeft = '16vw';
+
+            sdControl.style.gridTemplateColumns = '17% 64% 25%';
+
+            menu.style.fontSize = 'x-small';
+
+            intro3D.style.marginTop = '2em';
+            intro3D.style.display = 'grid';
+            intro3D.style.gridTemplateRows = '55%';
+            intro3D.style.height = 'auto';
+            intro3D.style.maxHeight = '100vh';
+
+            infoMenu.style.maxWidth = '100vw';
+            infoMenu.style.gridTemplateColumns = '45% 50%';
+            infoMenu.style.marginTop = '3vh';
+            infoMenu.style.marginBottom = '1vh';
+            infoMenu.style.height = '5vh';
+            infoMenu.style.display = 'grid';
+            infoMenu.style.alignContent = 'space-between';
+            infoMenu.style.justifyContent = 'space-evenly';
+            infoMenu.style.alignItems = 'center';
+            infoMenu.style.justifyItems = 'stretch';
+            infoMenu.style.marginLeft = '50px';
+
       return true;
     } else {
-      console.log("No estás usando una tablet o el dispositivo está en modo horizontal.");
-      return false;
     }
   }
   
-  // Llamar a la función para hacer la detección
   isTabletOrIpadInPortrait();
-  
-  // Escuchar cambios de orientación
   window.addEventListener("orientationchange", isTabletOrIpadInPortrait);
   
