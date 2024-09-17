566a5a528aff76af45c34fd614bc3dd7556de3ad
D0NN4 <sgarbidonna@gmail.com>
2024-09-17 00:07:03 -0300

    rotation ipad con alert

--- public/contenido/3Dconfiguration/ipad-vertical.js
+++ public/contenido/3Dconfiguration/ipad-vertical.js
@@ -1,15 +1,13 @@
 function isTabletOrIpadInPortrait() {
     // Detectar si es un iPad o un dispositivo con pantalla táctil
     const isIpadOrTablet = /iPad|Android/.test(navigator.userAgent) || (navigator.maxTouchPoints && navigator.maxTouchPoints > 1);
-  
-    // Verificar si la orientación es vertical (portrait)
     const isPortrait = window.matchMedia("(orientation: portrait)").matches;
 
-    // Opción alternativa para detectar orientación
-    // const isPortrait = window.orientation === 0 || window.orientation === 180;
+    // opcion alternativa p ver si estoy o no con portrait const isPortrait = window.orientation === 0 || window.orientation === 180;
   
     if (isIpadOrTablet && isPortrait) {
       console.log("Estás utilizando una tablet o iPad en modo vertical.");
+      alert('si');
       return true;
     } else {
       console.log("No estás usando una tablet o el dispositivo está en modo horizontal.");
